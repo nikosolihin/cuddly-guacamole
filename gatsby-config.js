a11fff1ca417d4349ec5a6e0233748aba62bf9d7
Niko Solihin <nikosolihin@gmail.com>
2018-08-27 14:00:41 +0700

    add env and config files

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'YICF Giving Page',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-emotion'],
+};
